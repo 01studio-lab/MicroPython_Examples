db85ce91501954f6821c783dbee72478b93bb53d
CaptainJackey <237827161@qq.com>
2020-12-02 11:15:12 +0800

    add st7735s 1.8' lcd

--- 1.pyBoard v1.1(STM32)/3.拓展实验/3.LCD液晶屏显示/boot.py
+++
@@ -1,8 +0,0 @@
-# boot.py -- run on boot-up
-# can run arbitrary Python, but best to keep it minimal
-
-import machine
-import pyb
-#pyb.main('main.py') # main script to run after this one
-#pyb.usb_mode('VCP+MSC') # act as a serial and a storage device
-#pyb.usb_mode('VCP+HID') # act as a serial device and a mouse
